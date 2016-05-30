90cbf63f3bab9e914294e0b9032708262dc01b50
Rafael Rezende <rafaelrezend@gmail.com>
2016-05-30 23:54:12 +0200

    Hiding Start Team Build feature.

--- com.ibm.team.build.hjplugin/src/main/java/jenkins/plugins/teamant/build/tasks/RTCStartTeamBuild.java
+++ com.ibm.team.build.hjplugin/src/main/java/jenkins/plugins/teamant/build/tasks/RTCStartTeamBuild.java
@@ -51,7 +51,7 @@ public class RTCStartTeamBuild extends Builder {
 				@SuppressWarnings("rawtypes") Class<? extends AbstractProject> aClass) {
 			// indicates that this builder can be used with all kinds of project
 			// types
-			return true;
+			return false;
 		}
 
 		/**
