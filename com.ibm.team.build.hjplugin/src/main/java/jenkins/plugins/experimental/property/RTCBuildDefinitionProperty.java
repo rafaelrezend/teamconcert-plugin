f47deef2e21053a38a39a3b81a9411f3e836228f
Rafael Rezende <rafaelrezend@gmail.com>
2016-05-30 23:53:44 +0200

    Adding RTC Build Definition as build wrapper.

--- com.ibm.team.build.hjplugin/src/main/java/jenkins/plugins/experimental/property/RTCBuildDefinitionProperty.java
+++ com.ibm.team.build.hjplugin/src/main/java/jenkins/plugins/experimental/property/RTCBuildDefinitionProperty.java
@@ -1,14 +1,25 @@
 package jenkins.plugins.experimental.property;
 
+import java.io.IOException;
+
 import org.kohsuke.stapler.DataBoundConstructor;
 
+import hudson.EnvVars;
 import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
 import hudson.model.BuildListener;
+import hudson.model.TaskListener;
 import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
 import hudson.model.Job;
+import hudson.model.Run;
+import hudson.tasks.BuildWrapper;
+import hudson.tasks.BuildWrapperDescriptor;
 import jenkins.model.OptionalJobProperty;
+import jenkins.tasks.SimpleBuildWrapper;
 
-public class RTCBuildDefinitionProperty extends OptionalJobProperty<Job<?,?>> {
+public class RTCBuildDefinitionProperty extends SimpleBuildWrapper {
 	
 	private boolean specified;
 	
@@ -17,30 +28,28 @@ public class RTCBuildDefinitionProperty extends OptionalJobProperty<Job<?,?>> {
 		// TODO Auto-generated constructor stub
 	}
 
-
-
-	/* (non-Javadoc)
-	 * @see hudson.model.JobProperty#prebuild(hudson.model.AbstractBuild, hudson.model.BuildListener)
-	 */
-	@Override
-	public boolean prebuild(AbstractBuild<?, ?> build, BuildListener listener) {
-		// TODO Auto-generated method stub
-		
-		listener.getLogger().println("##### rtc build def property, pre build");
-		
-		return super.prebuild(build, listener);
-	}
-	
-	
-
 	@Extension
-    public static class DescriptorImpl extends OptionalJobPropertyDescriptor {
+    public static class DescriptorImpl extends BuildWrapperDescriptor {
 
         @Override
         public String getDisplayName() {
             return "Connect to RTC Build Definition";
         }
 
+		@Override
+		public boolean isApplicable(AbstractProject<?, ?> arg0) {
+			// TODO Auto-generated method stub
+			return true;
+		}
+
     }
+
+	@Override
+	public void setUp(Context arg0, Run<?, ?> arg1, FilePath arg2, Launcher arg3,
+			TaskListener arg4, EnvVars arg5) throws IOException,
+			InterruptedException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
