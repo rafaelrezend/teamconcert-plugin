43049469b4b81e75eced0cc2fc54b1e214aa8813
Rafael Rezende <rafaelrezend@gmail.com>
2016-06-24 15:08:45 +0200

    Added server credentials mock to Build Definition selector.

--- com.ibm.team.build.hjplugin/src/main/java/jenkins/plugins/experimental/property/RTCBuildDefinitionProperty.java
+++ com.ibm.team.build.hjplugin/src/main/java/jenkins/plugins/experimental/property/RTCBuildDefinitionProperty.java
@@ -2,21 +2,28 @@ package jenkins.plugins.experimental.property;
 
 import java.io.IOException;
 
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import com.cloudbees.plugins.credentials.domains.URIRequirementBuilder;
+import com.ibm.team.build.internal.hjplugin.RTCBuildToolInstallation;
 
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
-import hudson.model.BuildListener;
 import hudson.model.TaskListener;
-import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.Job;
 import hudson.model.Run;
-import hudson.tasks.BuildWrapper;
+import hudson.security.ACL;
 import hudson.tasks.BuildWrapperDescriptor;
-import jenkins.model.OptionalJobProperty;
+import hudson.util.ListBoxModel;
 import jenkins.tasks.SimpleBuildWrapper;
 
 public class RTCBuildDefinitionProperty extends SimpleBuildWrapper {
@@ -27,7 +34,11 @@ public class RTCBuildDefinitionProperty extends SimpleBuildWrapper {
 	public RTCBuildDefinitionProperty() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public String getCredentialsId() {
+		return "rezende/******";
+	}
+	
 	@Extension
     public static class DescriptorImpl extends BuildWrapperDescriptor {
 
@@ -41,6 +52,45 @@ public class RTCBuildDefinitionProperty extends SimpleBuildWrapper {
 			// TODO Auto-generated method stub
 			return true;
 		}
+		
+		/**
+		 * Provides a listbox of the defined build tools to pick from. Also includes
+		 * an entry to signify no toolkit is chosen.
+		 * @return The valid build tool options
+		 */
+		public ListBoxModel doFillBuildToolItems() {
+			ListBoxModel listBox = new ListBoxModel();
+//			listBox.add(new ListBoxModel.Option(Messages.RTCScm_no_build_tool_name(), ""));
+			RTCBuildToolInstallation[] allTools = RTCBuildToolInstallation.allInstallations();
+			for (RTCBuildToolInstallation tool : allTools) {
+				ListBoxModel.Option option = new ListBoxModel.Option(tool.getName());
+				listBox.add(option);
+			}
+			return listBox;
+		}
+		
+		public ListBoxModel doFillCredentialsIdItems(@AncestorInPath Job<?, ?> project, @QueryParameter String serverURI) {
+			return new StandardListBoxModel()
+			.withEmptySelection()
+			.withMatching(CredentialsMatchers.instanceOf(StandardUsernamePasswordCredentials.class),
+					CredentialsProvider.lookupCredentials(StandardUsernamePasswordCredentials.class, project, ACL.SYSTEM, URIRequirementBuilder.fromUri(serverURI).build()));
+
+			// TODO look into us being able to support certificates
+//			return new StandardListBoxModel()
+//			.withEmptySelection()
+//			.withMatching(CredentialsMatchers.anyOf(
+//					CredentialsMatchers.instanceOf(StandardUsernamePasswordCredentials.class),
+//					CredentialsMatchers.instanceOf(StandardCertificateCredentials.class)),
+//					CredentialsProvider.lookupCredentials(StandardUsernamePasswordCredentials.class, project, ACL.SYSTEM, URIRequirementBuilder.fromUri(url).build()));
+		}
+		
+		public String getGlobalServerURI() {
+			return "https://192.168.99.100:9443/ccm";
+		}
+		
+		public int getGlobalTimeout() {
+    		return 480;
+	    }
 
     }
 
